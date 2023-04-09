dad5c1b63356bc0ad96a11c46ca5fa3bc25039bc
AxelKoerner <91koax1bif@hft-stuttgart.de>
2023-04-09 19:23:31 +0200

    introduced top bar, functionality not yet implemented

--- my-app/src/scenes/global/Topbar.tsx
+++ my-app/src/scenes/global/Topbar.tsx
@@ -1,22 +1,43 @@
 import NightsStayIcon from '@mui/icons-material/NightsStay';
 import LightModeIcon from '@mui/icons-material/LightMode';
-import {Box, IconButton, useTheme} from "@mui/material";
-import {ColorModeContext} from "../../theme";
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import SettingsIcon from '@mui/icons-material/Settings';
+import SearchIcon from '@mui/icons-material/Search';
+import {Box, IconButton, InputBase, useTheme} from "@mui/material";
+import {ColorModeContext, tokens} from "../../theme";
 import {useContext} from "react";
 
 const Topbar = () => {
     const theme = useTheme();
     const colorMode = useContext(ColorModeContext);
-    return (<Box display="flex" justifyContent="center" p={2}>
-
+    const colors = tokens(theme.palette.mode);
+    return (<Box display="flex" justifyContent="space-between" p={2}>
+        {/* SEARCH BAR */}
+        <Box sx={{
+            display: 'flex',
+            boxShadow: 1,
+            backgroundColor: colors.primary["400"]
+        }}>
+            <InputBase sx={{ml: 2, flex: 1}} placeholder="Search"/>
+            <IconButton type={"button"} sx={{p: 1}}>
+                <SearchIcon/>
+            </IconButton>
+        </Box>
         {/* ICONS */}
-        <Box display="flex">
+
+        <Box display={"flex"}>
             <IconButton onClick={colorMode.toggleColorMode}>
                 {theme.palette.mode === "dark" ? (
                     <NightsStayIcon/>
                 ) : (
                     <LightModeIcon/>
                 )}
+            </IconButton>
+            <IconButton>
+                <SettingsIcon/>
+            </IconButton>
+            <IconButton>
+                <AccountBoxIcon/>
             </IconButton>
         </Box>
     </Box>)
