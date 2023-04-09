fdf943a9899fbe237c122300c4155091c01765b0
AxelKoerner <91koax1bif@hft-stuttgart.de>
2023-04-09 15:51:29 +0200

    implemented simple toggle for light/dark mode and the required color themes

---
+++ my-app/src/scenes/global/Topbar.tsx
@@ -0,0 +1,26 @@
+import NightsStayIcon from '@mui/icons-material/NightsStay';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import {Box, IconButton, useTheme} from "@mui/material";
+import {ColorModeContext, tokens} from "../../theme";
+import {useContext} from "react";
+
+const Topbar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+    return (<Box display="flex" justifyContent="center" p={2}>
+
+        {/* ICONS */}
+        <Box display={"flex"}>
+            <IconButton onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode === "dark" ? (
+                    <NightsStayIcon/>
+                ) : (
+                    <LightModeIcon/>
+                )}
+            </IconButton>
+        </Box>
+    </Box>)
+};
+
+export default Topbar;
