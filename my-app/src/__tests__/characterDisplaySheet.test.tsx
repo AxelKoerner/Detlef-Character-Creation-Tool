4b9441a79ee4c2071c32e30ac55629d91ef3e403
Lukas_Weiland <91welu1bif@hft-stuttgart.de>
2023-06-22 00:55:48 +0200

    characterDisplay + customBackground tests

--- my-app/src/__tests__/characterDisplaySheet.test.tsx
+++ my-app/src/__tests__/characterDisplaySheet.test.tsx
@@ -1,7 +1,44 @@
-import characterDisplaySheet from "../scenes/CharacterSheet/CharacterDisplaySheet";
+import CharacterSheet from "../scenes/CharacterSheet/CharacterDisplaySheet";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 
-describe('Character Display Sheet', () => {
-    test('First Test', () => {
 
-    })
-})
+describe('CharacterSheet', () => {
+  test('renders character name', () => {
+    render(<CharacterSheet />);
+    const characterNameElement = screen.getByText(/testName/i);
+    expect(characterNameElement).toBeInTheDocument();
+  });
+
+  test('fetches user name', async () => {
+    render(<CharacterSheet />);
+    const userNameElement = await screen.findByText(/testUserName/i);
+    expect(userNameElement).toBeInTheDocument();
+  });
+
+  test('fetches class', async () => {
+    render(<CharacterSheet />);
+    const classNameElement = await screen.findByText(/testClassName/i);
+    expect(classNameElement).toBeInTheDocument();
+  });
+
+  test('fetches character level', async () => {
+    render(<CharacterSheet />);
+    const characterLevelElement = await screen.findByText(/testCharacterLevel/i);
+    expect(characterLevelElement).toBeInTheDocument();
+  });
+
+  test('fetches lineage', async () => {
+    render(<CharacterSheet />);
+    const lineageElement = await screen.findByText(/testLineage/i);
+    expect(lineageElement).toBeInTheDocument();
+  });
+
+  test('fetches background', async () => {
+    render(<CharacterSheet />);
+    const backgroundElement = await screen.findByText(/testBackground/i);
+    expect(backgroundElement).toBeInTheDocument();
+  });
+
+  
+});
--- my-app/src/__tests__/customBackground.test.tsx
+++ my-app/src/__tests__/customBackground.test.tsx
@@ -1,9 +1,68 @@
-import {render} from '@testing-library/react'
-import CustomBackground from "../scenes/Custom/CustomBackground";
 
-describe('Custom Background', () => {
-    test('First Test', () => {
-        render(<CustomBackground/>);
+import BackgroundForm from "../scenes/Custom/CustomBackground";
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
-    })
-})
+
+
+describe('BackgroundForm', () => {
+  beforeEach(() => {
+    render(<BackgroundForm />);
+  });
+
+  it('should render the background name input field', () => {
+    const backgroundNameInput = screen.getByLabelText(/Background Name/i) as HTMLInputElement;
+    expect(backgroundNameInput).toBeInTheDocument();
+  });
+
+  it('should update the background name state on input change', () => {
+    const backgroundNameInput = screen.getByLabelText(/Background Name/i) as HTMLInputElement;
+    fireEvent.change(backgroundNameInput, { target: { value: 'Test Background' } });
+    expect(backgroundNameInput.value).toBe('Test Background');
+  });
+
+  it('should render the ability name input field', () => {
+    const abilityNameInput = screen.getByLabelText(/Ability Name/i) as HTMLInputElement;
+    expect(abilityNameInput).toBeInTheDocument();
+  });
+
+  it('should update the ability name state on input change', () => {
+    const abilityNameInput = screen.getByLabelText(/Ability Name/i) as HTMLInputElement;
+    fireEvent.change(abilityNameInput, { target: { value: 'Test Ability' } });
+    expect(abilityNameInput.value).toBe('Test Ability');
+  });
+
+  it('should render the ability value input field', () => {
+    const abilityValueInput = screen.getByLabelText(/Ability Value/i) as HTMLInputElement;
+    expect(abilityValueInput).toBeInTheDocument();
+  });
+
+  it('should update the ability value state on input change', () => {
+    const abilityValueInput = screen.getByLabelText(/Ability Value/i) as HTMLInputElement;
+    fireEvent.change(abilityValueInput, { target: { value: '10' } });
+    expect(abilityValueInput.value).toBe('10');
+  });
+
+  it('should render the equipment field inputs', () => {
+    const equipmentFieldInputs = screen.getAllByLabelText(/Equipment ID/i) as HTMLInputElement[];
+    expect(equipmentFieldInputs.length).toBeGreaterThan(0);
+  });
+
+  it('should add a new equipment field on "Add Equipment Field" button click', () => {
+    const addEquipmentFieldButton = screen.getByText(/Add Equipment Field/i);
+    fireEvent.click(addEquipmentFieldButton);
+    const equipmentFieldInputs = screen.getAllByLabelText(/Equipment ID/i) as HTMLInputElement[];
+    expect(equipmentFieldInputs.length).toBe(2);
+  });
+
+  it('should remove an equipment field on "Remove" button click', () => {
+    const removeEquipmentButton = screen.getByText(/Remove/i);
+    fireEvent.click(removeEquipmentButton);
+    const equipmentFieldInputs = screen.getAllByLabelText(/Equipment ID/i) as HTMLInputElement[];
+    expect(equipmentFieldInputs.length).toBe(0);
+  });
+  
+
+  
+
+});
