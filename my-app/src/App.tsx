a096910aa9907fc4926628f6605b77165c92d931
AxelKoerner <91koax1bif@hft-stuttgart.de>
2023-07-17 17:34:40 +0200

    removed testing files

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -13,7 +13,6 @@ import CustomBackground from './scenes/Custom/CustomBackground';
 import CustomLineage from './scenes/Custom/CustomLineage';
 import CharacterDisplaySheet from './scenes/CharacterSheet/CharacterDisplaySheet';
 import SideBar from "./scenes/global/SideBar";
-import CharacterSheet1 from './scenes/CharacterSheet/characterSheet1';
 
 function App() {
     const [theme, colorMode] = useMode();
@@ -37,7 +36,6 @@ function App() {
                                 <Route path="/customBackground" element={<CustomBackground/>}/>
                                 <Route path="/customLineage" element={<CustomLineage/>}/>
                                 <Route path="/characterDisplaySheet" element={<CharacterDisplaySheet/>}/>
-                                <Route path="/characterSheet1" element={<CharacterSheet1/>}/>
                             </Routes>
 
                     </main>
